69da18b56fa4cf2ca77e07e185d1a023d94d5389
BomScoob12 <sorawitkraukham@gmail.com>
2023-03-31 22:45:54 +0700

    last test

--- AdvClass2/src/main/java/myproject/MyProject2.java
+++ AdvClass2/src/main/java/myproject/MyProject2.java
@@ -4,5 +4,6 @@ public class MyProject2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("test");
+        System.out.println("test eakteesii");
     }
 }
