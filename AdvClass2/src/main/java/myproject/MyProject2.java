45a7e91c20ef7dd949f591f1a1558feb3e3fabb2
BomScoob12 <sorawitkraukham@gmail.com>
2023-03-31 22:52:24 +0700

    Revert "test2"
    This reverts commit 1d7f0080990b3aef51d86ec2b3929ca96cbdeacc.

--- AdvClass2/src/main/java/myproject/MyProject2.java
+++ AdvClass2/src/main/java/myproject/MyProject2.java
@@ -3,7 +3,5 @@ package myproject;
 public class MyProject2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("test");
-        System.out.println("test eakteesii");
     }
 }
