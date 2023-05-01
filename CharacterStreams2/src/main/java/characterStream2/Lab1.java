3dccc1963da880a663c22f6a0064d2a906f7e854
BomScoob <sorawitkraukham@gmail.com>
2023-05-01 16:24:54 +0700

    Character all lab but Lab2 not finish yet

---
+++ CharacterStreams2/src/main/java/characterStream2/Lab1.java
@@ -0,0 +1,26 @@
+
+package characterStream2;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class Lab1 {
+    public static void main(String[] args) {
+        try{
+            FileWriter out = new FileWriter("Data1.txt");
+            out.write("Hello world");
+            out.write("Welcome to JAVA");
+            out.close();
+        }catch (IOException e){
+            Logger.getLogger(Lab1.class.getName()).log(Level.SEVERE, null, e);
+        }
+        try{
+            FileReader in = new FileReader("Data1.txt");
+            int ch;
+            while ((ch = in.read()) != -1){
+                System.out.println((char) ch);
+            }
+        }catch (IOException e){
+            Logger.getLogger(Lab1.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+}
